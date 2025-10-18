0d7f381eb67a9388a0719389c4d4ac5f350ced14
dev-praveen <spraveen0244@gmail.com>
2025-10-18 14:38:39 +0530

    added postgres database

--- src/main/java/com/auth/provider/LoginProviderApplication.java
+++ src/main/java/com/auth/provider/LoginProviderApplication.java
@@ -2,11 +2,19 @@ package com.auth.provider;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class LoginProviderApplication {
 
   public static void main(String[] args) {
     SpringApplication.run(LoginProviderApplication.class, args);
+  }
+
+  @RequestMapping("/hello")
+  public String hello() {
+    return "hello world " + Thread.currentThread().getName();
   }
 }
