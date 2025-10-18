daf9f0f07e869db410bd0e77a7477795c9acb1c8
dev-praveen <spraveen0244@gmail.com>
2025-10-18 12:05:14 +0530

    initial commit

---
+++ src/main/java/com/auth/provider/LoginProviderApplication.java
@@ -0,0 +1,12 @@
+package com.auth.provider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginProviderApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(LoginProviderApplication.class, args);
+  }
+}
---
+++ src/test/java/com/auth/provider/LoginProviderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.auth.provider;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginProviderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
